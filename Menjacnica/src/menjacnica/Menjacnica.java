ae06edda247db4ad12b71a719feaa090772df9d0
MomciloBogojevic2801 <momcilobogojevic2801@gmail.com>
2018-03-21 16:38:10 +0100

    Uvedena klasa Menjacnica koja implementira MenjacnicaInterfejs

---
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -0,0 +1,25 @@
+package menjacnica;
+
+import menjacnica.interfejs.MenjacnicaInterfejs;
+
+public class Menjacnica implements MenjacnicaInterfejs {
+
+	@Override
+	public void dodajKurs(String kod, double prodajniKurs, double kupovniKurs, double srednjiKurs) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void obrisiKurs(String kod) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public KursnaLista vratiKurs(String kod) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
