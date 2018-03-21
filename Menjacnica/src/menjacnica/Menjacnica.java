73c16a0ad4ebf17887fbcb50c44934b448802e38
MomciloBogojevic2801 <momcilobogojevic2801@gmail.com>
2018-03-21 22:01:18 +0100

    Izmenjena metoda dodajKurs u klasi Menjacnica

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -1,25 +1,49 @@
 package menjacnica;
 
+import java.util.*;
+
 import menjacnica.interfejs.MenjacnicaInterfejs;
 
 public class Menjacnica implements MenjacnicaInterfejs {
+	
+	public LinkedList<Valuta> kursnaLista = new LinkedList<>();
 
 	@Override
-	public void dodajKurs(String kod, double prodajniKurs, double kupovniKurs, double srednjiKurs) {
-		// TODO Auto-generated method stub
+	public void dodajKurs(String kod, String naziv, double prodajniKurs, double kupovniKurs) {
+		Valuta v = new Valuta();
+		v.setNaziv(naziv);
+		v.setKod(kod);
+		v.setProdajniKurs(prodajniKurs);
+		v.setKupovniKurs(kupovniKurs);
+		double srednjiKurs = (kupovniKurs + prodajniKurs)/2 ;
+		
+		v.setSrednjiKurs(srednjiKurs);
+		GregorianCalendar g = new GregorianCalendar();
+		v.setDatum(g);
+		
+		
 
+		kursnaLista.add(v);
+		System.out.println("Uneto je: " + v.toString());
 	}
 
 	@Override
 	public void obrisiKurs(String kod) {
-		// TODO Auto-generated method stub
+		for (int i = 0; i < kursnaLista.size(); i++) {
+			if (kursnaLista.get(i).getKod() == kod)
+				kursnaLista.remove(i);
+		}
 
 	}
 
 	@Override
-	public KursnaLista vratiKurs(String kod) {
-		// TODO Auto-generated method stub
-		return null;
+	public Valuta vratiKurs(String kod) {
+		for (int i = 0; i < kursnaLista.size(); i++) {
+			if (kursnaLista.get(i).getKod() == kod)
+				return kursnaLista.get(i);
+		}
+		throw new MenjacnicaException("Nema kursa za taj kod!");
+	//	return null;
 	}
 
 }
